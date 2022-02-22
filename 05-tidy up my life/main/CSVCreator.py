ca17b6dc86f5ad295b55523a94f4fa88d3adbc43
mahdirashidi998 <mahdirashidi998@gmail.com>
2022-02-22 10:05:13 +0100

    some debugging in CSVCreator

--- 05-tidy up my life/main/CSVCreator.py
+++ 05-tidy up my life/main/CSVCreator.py
@@ -16,35 +16,32 @@ def  CSVsNDays(N): #creates month directory, in each week directories and in eac
     for k in range(24) :
         hours = hours + str(k) + ','
     dates = createNDaysList(N) 
-    while x <= N : #creates directories of year
+    while x < N : #creates directories of year
         try : 
-            os.mkdir('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}'.format(year = dates[x].year))
-            x = x + 365 
+            os.mkdir('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}'.format(year = dates[x].year))     
         except :
-            continue
-    while j <= N : #creates directories of month
+            pass
+ #creates directories of month
         try : 
-            os.mkdir('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}\{month}'.format(year =dates[j].year, month = dates[j].month))
-            j = j + 28 
+            os.mkdir('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}\{month}'.format(year =dates[x].year, month = dates[x].month))
         except :
-            continue
-    while i <= N :#creates directories of week
+            pass
+#creates directories of week
         try : 
-            os.mkdir('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}\{month}\{week}'.format(year =dates[i].year, month = dates[i].month, week = dates[i].isocalendar().week))
-            i = i + 7
+            os.mkdir('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}\{month}\{week}'.format(year =dates[x].year, month = dates[x].month, week = dates[x].isocalendar().week))
         except :
-            continue
-    for d in dates : #creates csv files in appropriate directory
-        try :
-            f = open('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}\{month}\{week}\{day}.csv'.format(year = d.year, month = d.month, week = d.isocalendar().week, day = d.day),'w')
-            f.write(d.isoformat() + ',')
-            f.write(hours)
-            f.close()
-        except :
-            f = open('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}\{month}\{week}\{day}.csv'.format(year = d.year, month = d.month-1, week = d.isocalendar().week, day = d.day),'w')
-            f.write(d.isoformat() + ',')
-            f.write(hours)
-            f.close()
+            pass
+#creates csv files in appropriate directory
+        f = open('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}\{month}\{week}\{day}.csv'.format(year =dates[x].year, month = dates[x].month, week = dates[x].isocalendar().week, day = dates[x].day ),'w')
+        f.write(dates[x].isoformat() + ',')
+        f.write(hours)
+        f.close()
+        x = x +1
+
+         #   f = open('G:\My Drive\jahad\purposes\\find_job\data science projects\\05-tidy up my life\CSVs\{year}\{month}\{week}\{day}.xlsx'.format(year = d.year, month = d.month-1, week = d.isocalendar().week, day = d.day),'w')
+          #  f.write(d.isoformat() + ',')
+           # f.write(hours)
+            #f.close()
 CSVsNDays(100)
 
 
